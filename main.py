2808cee388d4128b205baf94a4447d6b483f9188
Mihailo Benington <mihailo.benington@gmail.com>
2024-06-19 19:36:34 +0200

    Modified main function to not pass arguments to SImulationHelper constructor, as those parameters are now constants

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from DTO.Fly import *
 from Helpers.CalculationHelper import CalculationHelper
 from Helpers.PlotHelper import *
-from Helpers.DataGenerator import *
+from Helpers.DataGeneratorHelper import *
 from Helpers.SimulationHelper import *
+from Helpers.ConstantHelper import *
 import pandas as pd
 
 def main():
-    simulation = SimulationHelper(flyNumber=5, stepNumber=700, stepSize=0.1, arenaRadius=0.5, distanceThreshold = 0.2, shouldPlot=False)
+    simulation = SimulationHelper()
     simulation.generateWalks()
     simulation.exportAll()
-
-    if simulation.shouldPlot:
-        simulation.plotFlies()
-        
-    simulation.exportAllFlyInteractions()
+    simulation.exportAllFlyInteractions()  
 
 
 if __name__=="__main__":
