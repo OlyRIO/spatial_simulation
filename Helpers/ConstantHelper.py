97c8aeb681160dc57fdbf8f08005368790423779
Mihailo Benington <mihailo.benington@gmail.com>
2024-07-02 18:49:04 +0200

    Added new constants representing the actual arena radius in millimeters and the scale between the real arena radius and scaled arena radius.

--- Helpers/ConstantHelper.py
+++ Helpers/ConstantHelper.py
@@ -1,6 +1,26 @@
-ARENA_RADIUS = 0.5
-FLY_NUMBER = 1
-STEP_NUMBER = 500
-STEP_SIZE = 0.1
-DISTANCE_THRESHHOLD = 0.2
-SHOULD_PLOT = False
+# Radius of the arena scaled down to <0, 1> millimeters 
+ARENA_RADIUS_SCALED = 0.5
+
+# Radius of the arena in milimeters
+ARENA_RADIUS_REAL = 19.417
+
+# Scaling ratio
+SCALING_RATIO = ARENA_RADIUS_SCALED / ARENA_RADIUS_REAL
+
+# Number of flies being simulated
+FLY_NUMBER = 5
+
+# Number of steps each flies makes
+STEP_NUMBER = 10000
+
+# Size of each step
+# STEP_SIZE = 0.1
+
+# Minimum distance between two flies required to categorise it as an interaction 
+INTERACTION_DISTANCE_THRESHHOLD = 0.2
+
+# Whether or not the application should show plots
+SHOULD_PLOT = True
+
+# Exports the animation if set to true 
+SHOULD_ANIMATE = False
